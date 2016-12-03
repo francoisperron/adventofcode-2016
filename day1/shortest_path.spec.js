eeca8dcc8d2ec3bd025f9b7bc8b7212ec9732620
Francois Perron <fperron@gmail.com>
2016-12-02 19:09:47 -0500

    mocha/chai works

---
+++ day1/shortest_path.spec.js
@@ -0,0 +1,8 @@
+import {describe, it} from "mocha";
+import {expect} from 'chai';
+
+describe('mocha works', () => {
+    it('true is true', () => {
+        expect(true).to.equal(true);
+    });
+});
