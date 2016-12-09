39d3b1ed011c5442e83e63b200ad7bdd2877fb87
Francois Perron <fperron@gmail.com>
2016-12-08 20:28:58 -0500

    day 7 part 2 done

--- day7/ipv7.js
+++ day7/ipv7.js
@@ -19,13 +19,20 @@ export class Ipv7 {
     }
 
     isTLS() {
-        const inAddress = this.addressParts.filter(a => patternIn(a) == true).length > 0;
-        const notInHNS = this.hyperNetSequences.filter(h => patternIn(h) == true).length == 0;
+        const inAddress = this.addressParts.filter(a => tlsPatternIn(a)).length > 0;
+        const notInHNS = this.hyperNetSequences.filter(h => tlsPatternIn(h)).length == 0;
         return inAddress && notInHNS;
+    }
+
+    isSSL() {
+        const ssl = this.addressParts.map(a => new SslPattern(a)).filter(s => s.isSsl());
+        const hyper = this.hyperNetSequences.map(h => new SslPattern(h)).filter(s => s.isSsl());
+        const reverse = ssl.map(s => s.reversePattern()).reduce((a,b) => a.concat(b), []);
+        return reverse.find(bab => hyper.find(h => h.pattern.includes(bab))) != undefined;
     }
 }
 
-export function patternIn(string) {
+export function tlsPatternIn(string) {
     const chars = string.split('');
     for (let i = 0; i < chars.length; i++) {
         if (chars[i] == chars[i + 3] && chars[i + 1] == chars[i + 2] && chars[i] != chars[i + 1]) return true;
@@ -33,6 +40,30 @@ export function patternIn(string) {
     return false;
 }
 
+export class SslPattern{
+    constructor(string){
+        this.pattern = [];
+        const chars = string.split('');
+        for (let i = 0; i < chars.length; i++) {
+            if (chars[i] == chars[i + 2] && chars[i] != chars[i + 1]) {
+                this.pattern.push(chars[i] + chars[i+1] + chars[i+2]);
+            }
+        }
+    }
+
+    isSsl(){
+        return this.pattern.length != 0;
+    }
+
+    reversePattern(){
+        return this.pattern.map(p => p[1] + p[0] + p[1]);
+    }
+}
+
 export function addressesSupportingTLS(addresses) {
-    return addresses.filter(a => new Ipv7(a).isTLS() == true).length;
+    return addresses.filter(a => new Ipv7(a).isTLS()).length;
+}
+
+export function addressesSupportingSSL(addresses) {
+    return addresses.filter(a => new Ipv7(a).isSSL()).length;
 }
--- day7/ipv7.spec.js
+++ day7/ipv7.spec.js
@@ -1,67 +1,124 @@
 import {describe, it} from "mocha";
 import {expect} from 'chai';
-import {patternIn, Ipv7, addressesSupportingTLS} from "./ipv7";
+import {tlsPatternIn, Ipv7, addressesSupportingTLS, sslPatternIn, SslPattern, addressesSupportingSSL} from "./ipv7";
 import {readLines} from "../read_file";
+import * as assert from "assert";
 
 describe('Day 7', () => {
     describe('detects Autonomous Bridge Bypass Annotation (ABBA) pattern', () => {
         it('in abba', () => {
-            expect(patternIn('abba')).to.equal(true);
+            expect(tlsPatternIn('abba')).to.equal(true);
         });
 
         it('not in baba', () => {
-            expect(patternIn('baba')).to.equal(false);
+            expect(tlsPatternIn('baba')).to.equal(false);
         });
 
         it('not in aaaa', () => {
-            expect(patternIn('aaaa')).to.equal(false);
+            expect(tlsPatternIn('aaaa')).to.equal(false);
         });
 
         it('in ioxxoj', () => {
-            expect(patternIn('ioxxoj')).to.equal(true);
+            expect(tlsPatternIn('ioxxoj')).to.equal(true);
+        });
+    });
+
+    describe('detects Area-Broadcast Accessor (ABA) pattern', () => {
+        it('in aba', () => {
+            const sslPattern = new SslPattern('aba');
+            expect(sslPattern.isSsl()).to.equal(true);
+            assert.deepEqual(sslPattern.reversePattern() ['bab']);
         });
 
-        describe('detects hypernet sequence (ABBA in [])', () => {
-            it('single sequence', () => {
-                expect(new Ipv7('abba[mnop]qrst').hyperNetSequences[0]).to.equal('mnop');
-            });
-
-            it('multiple sequences', () => {
-                const ipv7 = new Ipv7('abba[mnop]qrst[qwert]');
-                expect(ipv7.hyperNetSequences[0]).to.equal('mnop');
-                expect(ipv7.hyperNetSequences[1]).to.equal('qwert');
-            });
+        it('not in bba', () => {
+            const sslPattern = new SslPattern('bba');
+            expect(sslPattern.isSsl()).to.equal(false);
         });
 
-        describe('detects address parts (ABBA outside [])', () => {
-            it('single sequence', () => {
-                const ipv7 = new Ipv7('abba[mnop]qrst');
-                expect(ipv7.addressParts[0]).to.equal('abba');
-                expect(ipv7.addressParts[1]).to.equal('qrst');
-            });
+        it('not in aaa', () => {
+            const sslPattern = new SslPattern('aaa');
+            expect(sslPattern.isSsl()).to.equal(false);
         });
 
-        describe('detects TLS address', () => {
-            it('when it is', () => {
-                expect(new Ipv7('abba[mnop]qrst').isTLS()).to.equal(true);
-                expect(new Ipv7('ioxxoj[asdfgh]zxcvbn').isTLS()).to.equal(true);
-            });
-
-            it('when it is not', () => {
-                expect(new Ipv7('abcd[bddb]xyyx').isTLS()).to.equal(false);
-                expect(new Ipv7('aaaa[qwer]tyui').isTLS()).to.equal(false);
-            });
+        it('in ioxoj', () => {
+            const sslPattern = new SslPattern('ioxoj');
+            expect(sslPattern.isSsl()).to.equal(true);
+            assert.deepEqual(sslPattern.reversePattern(), ['xox']);
         });
 
-        describe('can solve day 7 part 1', () => {
-            it('dummy input', () => {
-                const dummyInput = ['abba[mnop]qrst','abcd[bddb]xyyx','aaaa[qwer]tyui','ioxxoj[asdfgh]zxcvbn'];
-                expect(addressesSupportingTLS(dummyInput)).to.equal(2);
-            });
+        it('in multiple patterns', () => {
+            const sslPattern = new SslPattern('gizvzrqjzhkikhb');
+            expect(sslPattern.isSsl()).to.equal(true);
+            assert.deepEqual(sslPattern.reversePattern(), ['vzv', 'iki']);
+        });
+    });
 
-            it('real input', () => {
-                expect(addressesSupportingTLS(readLines('day7/input.data'))).to.equal(110);
-            });
+
+    describe('detects hypernet sequence (ABBA in [])', () => {
+        it('single sequence', () => {
+            expect(new Ipv7('abba[mnop]qrst').hyperNetSequences[0]).to.equal('mnop');
+        });
+
+        it('multiple sequences', () => {
+            const ipv7 = new Ipv7('abba[mnop]qrst[qwert]');
+            expect(ipv7.hyperNetSequences[0]).to.equal('mnop');
+            expect(ipv7.hyperNetSequences[1]).to.equal('qwert');
+        });
+    });
+
+    describe('detects address parts (ABBA outside [])', () => {
+        it('single sequence', () => {
+            const ipv7 = new Ipv7('abba[mnop]qrst');
+            expect(ipv7.addressParts[0]).to.equal('abba');
+            expect(ipv7.addressParts[1]).to.equal('qrst');
+        });
+    });
+
+    describe('detects TLS address', () => {
+        it('when it is', () => {
+            expect(new Ipv7('abba[mnop]qrst').isTLS()).to.equal(true);
+            expect(new Ipv7('ioxxoj[asdfgh]zxcvbn').isTLS()).to.equal(true);
+        });
+
+        it('when it is not', () => {
+            expect(new Ipv7('abcd[bddb]xyyx').isTLS()).to.equal(false);
+            expect(new Ipv7('aaaa[qwer]tyui').isTLS()).to.equal(false);
+        });
+    });
+
+    describe('detects SSL address', () => {
+        it('when it is', () => {
+            expect(new Ipv7('aba[bab]xyz').isSSL()).to.equal(true);
+            expect(new Ipv7('aaa[kek]eke').isSSL()).to.equal(true);
+            expect(new Ipv7('zazbz[bzb]cdb').isSSL()).to.equal(true);
+            expect(new Ipv7('cdb[bzb]zbz').isSSL()).to.equal(true);
+            expect(new Ipv7('mbweohfcgybqcqnl[yafsvfrduertfqze]hqaodhzkmhzmlrxuc[bytcgnvzvoovirqwn]njivpwgkkqvgowpenh[erodavzscuubhea]gizvzrqjzhkikhb').isSSL()).to.equal(true);
+        });
+
+        it('when it is not', () => {
+            expect(new Ipv7('xyx[xyx]xyx').isSSL()).to.equal(false);
+        });
+    });
+
+    describe('can solve day 7 part 1', () => {
+        it('dummy input', () => {
+            const dummyInput = ['abba[mnop]qrst', 'abcd[bddb]xyyx', 'aaaa[qwer]tyui', 'ioxxoj[asdfgh]zxcvbn'];
+            expect(addressesSupportingTLS(dummyInput)).to.equal(2);
+        });
+
+        it('real input', () => {
+            expect(addressesSupportingTLS(readLines('day7/input.data'))).to.equal(110);
+        });
+    });
+
+    describe('can solve day 7 part 2', () => {
+        it('dummy input', () => {
+            const dummyInput = ['aba[bab]xyz', 'xyx[xyx]xyx', 'aaa[kek]eke', 'zazbz[bzb]cdb'];
+            expect(addressesSupportingSSL(dummyInput)).to.equal(3);
+        });
+
+        it('real input', () => {
+            expect(addressesSupportingSSL(readLines('day7/input.data'))).to.equal(242);
         });
     });
 });
