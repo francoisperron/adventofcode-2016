d884e1b0265c7e415809a2f9ac517dfb49eaa8e0
Francois Perron <fperron@gmail.com>
2016-12-15 08:26:55 -0500

    can determine if a string contains 3 repeating chars

--- day14/one-time-key.js
+++ day14/one-time-key.js
@@ -3,3 +3,8 @@ import crypto from 'crypto';
 export function generateKey(string) {
     return crypto.createHash('md5').update(string).digest('hex');
 }
+
+export function possibleKey(key) {
+    const char = key.match(/(.)\1{2}/);
+    return char == null ? undefined : char[1];
+}
--- day14/one-time-key.spec.js
+++ day14/one-time-key.spec.js
@@ -1,11 +1,15 @@
 import {describe, it} from 'mocha';
 import {expect} from 'chai';
-import {generateKey} from "./one-time-key";
+import {generateKey, possibleKey} from "./one-time-key";
 
 const dummyInput = 'abc';
 
 describe('Day 14', () => {
     it('Can generate lowercase hex md5 of salt+index', () => {
         expect(generateKey(dummyInput+18)).to.contains('cc38887a5');
+    });
+    it('Can determine if a string contains 3 repeating chars', () => {
+        expect(possibleKey(generateKey(dummyInput+18))).to.equal('8');
+        expect(possibleKey(generateKey(dummyInput+17))).to.equal(undefined);
     });
 });
