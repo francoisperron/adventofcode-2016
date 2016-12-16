aa3f8a98088838b326049de07ecdaa5568f267da
Francois Perron <fperron@gmail.com>
2016-12-15 20:35:42 -0500

    day 14 part 1 done

--- day14/one-time-key.js
+++ day14/one-time-key.js
@@ -1,14 +1,32 @@
 import crypto from 'crypto';
 
-export function generateKey(string) {
-    return crypto.createHash('md5').update(string).digest('hex');
-}
+export const generateKey = (string) => crypto.createHash('md5').update(string).digest('hex');
 
-export function possibleKey(key) {
-    const char = key.match(/(.)\1{2}/);
+export const containsTriplet = (key) => {
+    const threeChars = /(.)\1\1/;
+    const char = key.match(threeChars);
     return char == null ? undefined : char[1];
-}
+};
 
-export function validKey(key, char) {
-    return key.includes(char.repeat(5));
+export const containsQuintuplet = (key, char) => key.includes(char.repeat(5));
+
+export const validKey = (salt, index, char) => {
+    const nextThousand = Array.from(Array(1000).keys());
+    return nextThousand.some(i => containsQuintuplet(generateKey(salt + (index + i + 1)), char));
+};
+
+export function generateKeys(salt) {
+
+    let keys = [];
+    let index = 0;
+    while (keys.length < 64) {
+        const key = generateKey(salt + index);
+        const char = containsTriplet(key);
+
+        if (char != undefined && validKey(salt, index, char)) {
+            keys.push({key, char, index});
+        }
+        index++;
+    }
+    return keys;
 }
--- day14/one-time-key.spec.js
+++ day14/one-time-key.spec.js
@@ -1,19 +1,41 @@
 import {describe, it} from 'mocha';
 import {expect} from 'chai';
-import {generateKey, possibleKey, validKey} from "./one-time-key";
+import {generateKey, generateKeys, containsTriplet, containsQuintuplet, validKey} from "./one-time-key";
 
 const dummyInput = 'abc';
 
 describe('Day 14', () => {
     it('Can generate lowercase hex md5 of salt+index', () => {
-        expect(generateKey(dummyInput+18)).to.contains('cc38887a5');
+        expect(generateKey(dummyInput + 18)).to.contains('cc38887a5');
     });
     it('Can determine if a string contains 3 repeating char', () => {
-        expect(possibleKey(generateKey(dummyInput+18))).to.equal('8');
-        expect(possibleKey(generateKey(dummyInput+17))).to.equal(undefined);
+        expect(containsTriplet(generateKey(dummyInput + 18))).to.equal('8');
+        expect(containsTriplet(generateKey(dummyInput + 17))).to.equal(undefined);
     });
     it('Can determine if a string contains 5 repeating specific char', () => {
-        expect(validKey(generateKey(dummyInput+816), 'e')).to.equal(true);
-        expect(validKey(generateKey(dummyInput+17), '8')).to.equal(false);
+        expect(containsQuintuplet(generateKey(dummyInput + 816), 'e')).to.equal(true);
+        expect(containsQuintuplet(generateKey(dummyInput + 17), '8')).to.equal(false);
+    });
+    it('Validates if a key is valid looking at the next 1000 hash', () => {
+        expect(validKey(dummyInput, 18, '8')).to.equal(false);
+        expect(validKey(dummyInput, 39, 'e')).to.equal(true);
+    });
+
+    describe('Part 1', () => {
+        it('Generates 64th valid key at index 22728 for the dummy data', function () {
+            this.timeout(50000);
+            const keys = generateKeys(dummyInput);
+
+            expect(keys[63].index).to.equal(22728);
+            expect(keys[63].key).to.equal('26ccc731a8706e0c4f979aeb341871f0');
+        });
+
+        it('Real input', function () {
+            this.timeout(50000);
+            const keys = generateKeys('cuanljph');
+
+            expect(keys.length).to.equal(64);
+            expect(keys[63].index).to.equal(23769);
+        });
     });
 });
