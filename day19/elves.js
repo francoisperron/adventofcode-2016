51a121a5bb383ead7b8dc70f717de824c5e5683d
Francois Perron <fperron@gmail.com>
2016-12-19 20:07:47 -0500

    day 19 part 2 with jopesh help

--- day19/elves.js
+++ day19/elves.js
@@ -1,4 +1,4 @@
-export const elfWithAllPresents = (count) => {
+export const winnerPart1 = (count) => {
     let elves = Array.from(Array(count + 1).keys()).map(v => parseInt(v));
     elves.splice(0, 1);
 
@@ -12,3 +12,19 @@ export const elfWithAllPresents = (count) => {
 
     return elves[0];
 };
+
+
+export const josephusPart2 = (count) => {
+    const pow = Math.floor(Math.log(count) / Math.log(3));
+    const candidate = Math.pow(3, pow);
+
+    if (lastElf(count, candidate))
+        return candidate;
+
+    return inLowerPart(count, candidate) ?
+        count - candidate :
+        2 * count - 3 * candidate;
+};
+
+const lastElf = (count, candidate) => count == candidate;
+const inLowerPart = (count, candidate) => count - candidate <= candidate;
--- day19/elves.spec.js
+++ day19/elves.spec.js
@@ -1,39 +1,57 @@
 import {describe, it} from 'mocha';
 import {expect} from 'chai';
-import {elfWithAllPresents, nextElfWithGift} from "./elves";
+import {winnerPart1, josephusPart2} from "./elves";
 
-describe('Day 19', () => {
+desciribe('Day 19', () => {
     describe('The elf with all the gifts', () => {
         it('one elf with a gift won!', () => {
             const elvesCount = 1;
 
-            expect(elfWithAllPresents(elvesCount)).to.equal(1);
+            expect(winnerPart1(elvesCount)).to.equal(1);
         });
 
         it("elf one steals elf's two gift", () => {
             const elvesCount = 2;
 
-            expect(elfWithAllPresents(elvesCount)).to.equal(1);
+            expect(winnerPart1(elvesCount)).to.equal(1);
         });
 
         it("elf 3 steals gifts from two other elves", () => {
             const elvesCount = 3;
 
-            expect(elfWithAllPresents(elvesCount)).to.equal(3);
+            expect(winnerPart1(elvesCount)).to.equal(3);
         });
 
         it("dummy input", () => {
             const elvesCount = 5;
 
-            expect(elfWithAllPresents(elvesCount)).to.equal(3);
+            expect(winnerPart1(elvesCount)).to.equal(3);
         });
 
-        it("real input", function () {
-            this.timeout(30000);
+        it.skip("real input", () => {
             const elvesCount = 3012210;
 
-            expect(elfWithAllPresents(elvesCount)).to.equal(1830117);
+            expect(winnerPart1(elvesCount)).to.equal(1830117);
         });
     });
 
+    describe('Part 2', () => {
+
+        const input = [
+            {elves: 1, winner: 1},
+            {elves: 2, winner: 1},
+            {elves: 3, winner: 3},
+            {elves: 5, winner: 2},
+            {elves: 6, winner: 3},
+            {elves: 7, winner: 5},
+        ];
+        it("dummy input", () => {
+            input.forEach(i => expect(josephusPart2(i.elves)).to.equal(i.winner));
+        });
+        it("real input", () => {
+            const elvesCount = 3012210;
+
+            expect(josephusPart2(elvesCount)).to.equal(1417887);
+        });
+    });
 });
