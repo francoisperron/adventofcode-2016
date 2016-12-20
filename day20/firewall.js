f2d7b5dbcbd61c1cd8dc3fd9cf50e3fd29ce029f
Francois Perron <fperron@gmail.com>
2016-12-20 08:37:30 -0500

    day 20 part 2 done

--- day20/firewall.js
+++ day20/firewall.js
@@ -1,15 +1,22 @@
 export const parse = (input) => input.map(i => [parseInt(i.split('-')[0]), parseInt(i.split('-')[1])]);
 
-export const nonBlockedIp = (blacklist) => {
-    let ip = 0;
-    let found = false;
-    while (!found){
-        if(blacklist.find(b => b[0] <= ip && b[1] >= ip) == undefined){
-            found = true;
+export const orderAscending = (blacklist) => blacklist.sort((l1, l2) => l1[0] - l2[0]);
+
+export const nonBlockedIps = (blacklist, maxIps) => {
+    blacklist = orderAscending(blacklist);
+    let ips = 0;
+    let lastMaxIp = -1;
+    for (let list of blacklist) {
+        const minIp = Math.max(0, list[0]);
+        const allowedIps = minIp - lastMaxIp - 1;
+        if (allowedIps > 0) {
+            ips += allowedIps;
         }
-        ip++;
+        lastMaxIp = Math.max(lastMaxIp, list[1]);
     }
-    return --ip;
+    ips += maxIps - lastMaxIp > 0 ? maxIps - lastMaxIp : 0;
+
+    return ips;
 };
 
 
--- day20/firewall.spec.js
+++ day20/firewall.spec.js
@@ -1,10 +1,13 @@
 import {describe, it} from "mocha";
 import {expect} from 'chai';
-import {nonBlockedIp, merge, parse} from "./firewall";
+import {nonBlockedIp, merge, parse, orderAscending, nonBlockedIps} from "./firewall";
 import {readLines} from "../read_file";
 
-const dummyInput = ['5-8','0-2','4-7'];
+const dummyInput = ['5-8', '0-2', '4-7'];
+const dummyInputMax = 9;
+
 const realInput = readLines('day20/input.data');
+const realInputMax = 4294967295;
 
 describe('Day 20', () => {
     describe('Finds lowest non-blocked IP', () => {
@@ -18,12 +21,34 @@ describe('Day 20', () => {
         });
     });
 
+    describe('Finds allowed IPs count', () => {
+        it('Dummy input', () => {
+            const blackList = parse(dummyInput);
+            expect(nonBlockedIps(blackList, dummyInputMax)).to.equal(2);
+        });
+
+        it('Dummy input without zero', () => {
+            const noZeroInput = ['5-8', '4-7'];
+            const blackList = parse(noZeroInput);
+            expect(nonBlockedIps(blackList, dummyInputMax)).to.equal(5);
+        });
+
+        it('Real input', () => {
+            const blackList = parse(realInput);
+            expect(nonBlockedIps(blackList, realInputMax)).to.equal(146);
+        });
+    });
+
     describe('Parses input', () => {
         it('dummy input', () => {
-            expect(parse(dummyInput)).to.deep.equal([[5,8],[0,2],[4,7]]);
+            expect(parse(dummyInput)).to.deep.equal([[5, 8], [0, 2], [4, 7]]);
         });
         it('real input ', () => {
             expect(parse(realInput).length).to.equal(1176);
         });
+    });
+
+    it('Order in ascending order the blacklist', () => {
+        expect(orderAscending([[5, 8], [0, 2], [4, 7]])).to.deep.equal([[0, 2], [4, 7], [5, 8]])
     });
 });
