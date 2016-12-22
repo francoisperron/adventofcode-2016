e45ba9f96165272a7f8bee6b1d2884ff2bb7903a
Francois Perron <fperron@gmail.com>
2016-12-21 23:08:20 -0500

    day 21 can reverse and rotate

--- day21/scrambled-letters.js
+++ day21/scrambled-letters.js
@@ -5,12 +5,33 @@ const swapPosition = (ops, pwd) => {
 
 const swapLetter = (ops, pwd) => {
     const {l1, l2} = {l1: ops[2], l2: ops[5]};
-    return pwd.replace(new RegExp(l1,'g'), l2.toUpperCase()).replace(new RegExp(l2,'g'), l1).toLowerCase();
+    return pwd.replace(new RegExp(l1, 'g'), l2.toUpperCase()).replace(new RegExp(l2, 'g'), l1).toLowerCase();
+};
+
+const reverse = (ops, pwd) => {
+    const {from, to} = {from: parseInt(ops[2]), to: parseInt(ops[4])};
+    return pwd.slice(0, from) + pwd.slice(from, to + 1).split('').reverse().join('') + pwd.slice(to + 1, pwd.length);
+};
+
+const rotateLeft = (ops, pwd) => {
+    const nbTimes = Array.from(new Array(parseInt(ops[2])).keys());
+    return nbTimes.reduce(newPwd => newPwd.substring(1) + newPwd.charAt(0), pwd);
+};
+
+const rotateRight = (ops, pwd) => {
+    const nbTimes = Array.from(new Array(parseInt(ops[2])).keys());
+    return nbTimes.reduce(newPwd => newPwd.charAt(newPwd.length - 1) + newPwd.substring(0, newPwd.length - 1), pwd);
 };
 
 export const perform = (operation, pwd) => {
     const ops = operation.split(' ');
-    const select = {swapposition: swapPosition, swapletter: swapLetter};
+    const select = {
+        swapposition: swapPosition,
+        swapletter: swapLetter,
+        reversepositions: reverse,
+        rotateleft: rotateLeft,
+        rotateright: rotateRight,
+    };
 
     return select[ops[0] + ops[1]](ops, pwd);
 };
--- day21/scrambled-letters.spec.js
+++ day21/scrambled-letters.spec.js
@@ -15,5 +15,20 @@ describe('Day 21', () => {
             expect(perform('swap letter b with letter d', pwd)).to.equal('edcba');
             expect(perform('swap letter a with letter z', 'aazaaz')).to.equal('zzazza');
         });
+        it('solve reverse positions', () => {
+            const pwd = 'edcba';
+            expect(perform('reverse positions 0 through 4', pwd)).to.equal('abcde');
+            expect(perform('reverse positions 1 through 3', pwd)).to.equal('ebcda');
+        });
+        it('solve rotate left', () => {
+            const pwd = 'abcde';
+            expect(perform('rotate left 1 step', pwd)).to.equal('bcdea');
+            expect(perform('rotate left 2 step', pwd)).to.equal('cdeab');
+        });
+        it('solve rotate right', () => {
+            const pwd = 'abcde';
+            expect(perform('rotate right 1 step', pwd)).to.equal('eabcd');
+            expect(perform('rotate right 2 step', pwd)).to.equal('deabc');
+        });
     });
 });
